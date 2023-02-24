075c2abe5395ff077c4109fa5e458d6fc147ed54
Stanislav <gooftp@ya.ru>
2023-02-24 16:34:06 +0300

    h

---
+++ windows/thrift_based/setup_environment.bat.bat
@@ -0,0 +1,10 @@
+setX DG_ROOT "%~dp0" /M
+setX DG_BOOST_ROOT "D:\dev\external_dependencies\boost_1_70_0" /M
+setX DG_OPENSSL_ROOT "%~dp0libs\openssl-1.1.1c" /M
+setX DG_LIBEVENT_ROOT "%~dp0libs\libevent-2.1.8\src" /M
+setX DG_THRIFT_ROOT "%~dp0libs\thrift-0.12.0" /M
+setX DG_CRYPTOPP_ROOT "%~dp0libs\cryptopp-8.2" /M
+setX DG_ZLIB_ROOT "%~dp0libs\zlib-1.2.9" /M
+
+pause
+pause
